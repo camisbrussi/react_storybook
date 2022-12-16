1251083bce0714cdf4d2a41a839bc1f54f5cc42b
camisbrussi <camila.sbrussi@universo.univates.br>
2022-12-16 16:25:10 -0300

    created storybook

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,42 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae, accusamus. Eaque amet similique illo facilis tempora dignissimos, dolores adipisci aut, error maxime molestias consequuntur beatae aliquam minima architecto dolore excepturi.',
+    sizes: 'md',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
